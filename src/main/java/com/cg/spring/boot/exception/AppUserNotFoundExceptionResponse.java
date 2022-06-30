7213e322e9678756afd46182378fd4d77aa7e613
MaddinaSruthi <106723526+MaddinaSruthi@users.noreply.github.com>
2022-06-30 23:18:19 +0530

    Initial commit

---
+++ src/main/java/com/cg/spring/boot/exception/AppUserNotFoundExceptionResponse.java
@@ -0,0 +1,23 @@
+package com.cg.spring.boot.exception;
+
+/**
+ *	 UserNotFoundExceptionResponse is used to handle exceptions on user.
+ */
+public class AppUserNotFoundExceptionResponse {
+	
+	private String loginName;
+
+	public AppUserNotFoundExceptionResponse(String loginName) {
+		super();
+		this.loginName = loginName;
+	}
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}	
+
+}
